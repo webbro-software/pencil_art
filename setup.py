0e0abb229aeb744da8da123caaf19e70a9505f07
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-01-29 11:03:56 +0500

    fix: import issue

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pencil_art',
-    version='0.1.3',  
+    version='0.1.5',  
     packages=find_packages(),
     package_dir={'': 'src'},
     install_requires=[
