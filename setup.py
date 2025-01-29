bbe0703a433d353161652439e53d7d1e76a87d83
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-01-29 11:06:14 +0500

    fix: import issue

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pencil_art',
     version='0.1.5',  
-    packages=find_packages(),
+    packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
         'opencv-python',  
